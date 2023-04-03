ea24aa0ae35594221554a75f9daacc66720f5bea
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2023-04-03 16:13:02 -0400

    Add a deleteTodo action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,20 +1,37 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { ActionTypes } from "./types";
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   completed: boolean;
 }
 
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(
       "https://jsonplaceholder.typicode.com/todos"
     );
-    dispatch({
-      type: "FETCH_TODOS",
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: response.data,
     });
+  };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id,
   };
 };
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,41 @@
-import React from "react"
-export class App extends React.Component {
-    render() {
-        return (
-        <div>
-            <h1>
-                Hi There
-            </h1>
-        </div>
-        )
-    }
+import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
+import "../app.css";
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
 }
+
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div key={todo.id}>
+          <div>{todo.title}</div>
+          <button>DELETE</button>
+          <hr />
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        <div>{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
